bc5dcdbeff3144235c32acbb8e54b04aa5139f4a
Alexandr <45771430+Fluiser@users.noreply.github.com>
2023-08-23 03:31:01 +0300

    Update optimized_and_interface.cpp
    Надо внимательнее читать, что запускаешь. Это лучше не запускать.

--- optimized_and_interface.cpp
+++ optimized_and_interface.cpp
@@ -6,17 +6,17 @@
 #include <mutex>
 
 #ifndef MAX_INDEX
-#define MAX_INDEX (1024*200)
+#define MAX_INDEX (500)
 #endif
 #ifndef THREADS_SIZE
 #define THREADS_SIZE 8
 #endif
 
-#define NUMBER 409600
+#define NUMBER 1600
 
 #if ((NUMBER > 16000) || MAX_INDEX > 600)
 
-#warning "------------------- ALARM!!! ------------------------\nDefined values NUMBER and MAX_INDEX IS VERY DOHUYA\n-----------------------------------------------------"
+#warning "*****----> Defined values NUMBER and MAX_INDEX IS VERY DOHUYA"
 
 #endif
 
